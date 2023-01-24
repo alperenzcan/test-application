52d6429bc943bec7d460918da03172d8b153b9bd
Alperen <80932033+alperenzcan@users.noreply.github.com>
2023-01-25 02:39:01 +0300

    spring init

---
+++ testws/src/main/java/com/alperen/testws/TestwsApplication.java
@@ -0,0 +1,13 @@
+package com.alperen.testws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestwsApplication.class, args);
+	}
+
+}
---
+++ testws/src/test/java/com/alperen/testws/TestwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperen.testws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
